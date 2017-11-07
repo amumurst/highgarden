4e0aba49e15a5dcd5ffc5673f025115aab2f43c9
Amund Murstad <amund.murstad@gmail.com>
2017-11-07 22:38:54 +0100

    Format and bump core

--- src/test/scala/example/MainSpec.scala
+++ src/test/scala/example/MainSpec.scala
@@ -1,10 +1,5 @@
 package example
 
-import no.amumurst.Main
 import org.scalatest._
 
-class MainSpec extends FlatSpec with Matchers {
-  "The Hello object" should "say hello" in {
-    Main.greeting shouldEqual "hello"
-  }
-}
+class MainSpec extends FlatSpec with Matchers {}
