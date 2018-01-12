c623d2eb102c5188164369d6ad356eff41d2feed
Amund Murstad <amund.murstad@gmail.com>
2018-01-13 00:51:42 +0100

    Add explicit dependency on cats

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,33 +2,44 @@ import sbt._
 
 object Dependencies {
   private val http4sVersion = "0.18.0-M8"
-  private val doobieVersion = "0.5.0-M12"
+  private val doobieVersion = "0.5.0-M13"
   private val circeVersion  = "0.9.0"
+  private val catsVersion   = "1.0.1"
 
   lazy val scalaTest = "org.scalatest" %% "scalatest"   % "3.0.3"
   val logging        = "org.slf4j"     % "slf4j-simple" % "1.7.25"
 
-  val http4s = Seq(
-    "org.http4s" %% "http4s-dsl"          % http4sVersion,
-    "org.http4s" %% "http4s-blaze-server" % http4sVersion,
-    "org.http4s" %% "http4s-circe"        % http4sVersion,
-  )
+  val cats = Seq(
+    "org.typelevel" %% "cats-core",
+    "org.typelevel" %% "cats-macros",
+    "org.typelevel" %% "cats-kernel"
+  ).map(_ % catsVersion)
 
-  val database = Seq(
-    "com.opentable.components" % "otj-pg-embedded"  % "0.10.0",
-    "org.flywaydb"             % "flyway-core"      % "5.0.5",
-    "org.tpolecat"             %% "doobie-core"     % doobieVersion,
-    "org.tpolecat"             %% "doobie-postgres" % doobieVersion
-  )
+  val http4s = Seq(
+    "org.http4s" %% "http4s-dsl",
+    "org.http4s" %% "http4s-blaze-server",
+    "org.http4s" %% "http4s-circe",
+  ).map(_ % http4sVersion)
+
   val json = Seq(
     "io.circe" %% "circe-generic",
     "io.circe" %% "circe-literal"
   ).map(_ % circeVersion)
+
+  val doobie = Seq(
+    "org.tpolecat" %% "doobie-core",
+    "org.tpolecat" %% "doobie-postgres"
+  ).map(_ % doobieVersion)
+
+  val database = Seq(
+    "com.opentable.components" % "otj-pg-embedded" % "0.10.0",
+    "org.flywaydb"             % "flyway-core"     % "5.0.5"
+  ) ++ doobie
 
   val divDeps = Seq(
     logging
   )
 
   val testDeps = Seq(scalaTest).map(_ % Test)
-  val coreDeps = http4s ++ database ++ divDeps ++ json
+  val coreDeps = cats ++ http4s ++ database ++ divDeps ++ json
 }
